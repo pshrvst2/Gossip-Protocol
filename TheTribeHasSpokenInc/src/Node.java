afec5001eb7716a3662470ca146b5001f7b6ae07
kevincxm <kevincxm@gmail.com>
2015-10-04 20:02:03 -0500

    change the frequence of sending for testing

--- TheTribeHasSpokenInc/src/Node.java
+++ TheTribeHasSpokenInc/src/Node.java
@@ -117,7 +117,7 @@ public class Node
 			
 			// logic to send periodically
 			ScheduledExecutorService _schedulerService = Executors.newScheduledThreadPool(3);
-			_schedulerService.scheduleAtFixedRate(new SenderThread(_portReceiver), 0, 400, unit);
+			_schedulerService.scheduleAtFixedRate(new SenderThread(_portReceiver), 0, 1000, unit);
 			
 			// logic to scan the list and perform necessary actions.
 			_schedulerService.scheduleAtFixedRate(new ListScanThread(), 0, 100, unit);
